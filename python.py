1bd815b46a7a59f468a9b159175d89ac737e2436
Subhasish Swain <37550428+methedjangoguy@users.noreply.github.com>
2019-03-14 11:33:24 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,15 @@
+print("chopa")
+
+list = [1,2,3,4,5]
+print(list)
+
+a = 'rims'
+print(a)
+print(a*2)
+print(a*50)
+b = ['kana karuchu']
+print(b[:])
+c = [1,2,3,4,5,6,7]
+print(c[:4])
+print(c[2:5])
+print(c[2:])
